dda6d09f1c997104c3538cf3745cd1d31828f959
Mukul Chauhan <mukul.chauhan@hotmail.com>
2017-06-03 19:50:49 +0530

    initial commit

--- ImageAnalysis/StarCounter.py
+++ ImageAnalysis/StarCounter.py
@@ -1,1 +1,17 @@
+import numpy as np
+import cv2
 
+img = cv2.imread('stars.jpg',0)
+print(img)
+a,b = map(lambda x:x,list(np.shape(img)))
+print(a,b)
+for i in range(a):
+    for j in range(b):
+        if img[i][j] < 200:
+            img[i][j] = 0
+print(img)
+cv2.imshow("Image",img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
