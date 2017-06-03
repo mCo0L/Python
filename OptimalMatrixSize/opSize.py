71e88252a15ba3772ef80d425678dd00e5cc664d
Mukul Chauhan <mukul.chauhan@hotmail.com>
2017-06-03 20:47:59 +0530

    Logic update

--- OptimalMatrixSize/opSize.py
+++ OptimalMatrixSize/opSize.py
@@ -7,6 +7,8 @@ a = int(math.sqrt(n))+1
 while(a >= 1):
     if(n%a==0):
         b = n//a
+		if b>a:
+			a,b = b,a
         print("Optimal Matrix Dimensions are: ",a," X ",b)
         break
     else:
