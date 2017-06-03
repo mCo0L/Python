e99e6d1bb2653c7743ccd8af85f0ceadd4073b58
Mukul Chauhan <mCo0l@users.noreply.github.com>
2017-06-03 18:57:30 +0530

    Update opSize.py

--- OptimalMatrixSize/opSize.py
+++ OptimalMatrixSize/opSize.py
@@ -1,1 +1,13 @@
+import math
 
+print("Enter number of variables: ")
+n = int(input())
+
+a = int(math.sqrt(n))+1
+while(a >= 1):
+    if(n%a==0):
+        b = n//a
+        print("Optimal Matrix Size is: ",a," X ",b)
+        break
+    else:
+        a -= 1
